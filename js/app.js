0931e4c9fa136448f047adbabaed8edb4a72a107
saulotarsobc <saulotarsobc@gmail.com>
2021-11-27 12:38:45 -0300

    a

---
+++ js/app.js
@@ -0,0 +1,74 @@
+/* 11:02:16 - 27/11/21 */
+
+var data = [];
+
+var gatilhos = document.querySelectorAll('.gatilho');
+
+gatilhos.forEach(gatilho => {
+
+    gatilho.addEventListener('input', function() {
+
+        data[gatilho.id] = gatilho.value;
+
+        renderJson(data);
+
+    });
+
+});
+
+/* 11:14:09 - 27/11/21 */
+var code_json = document.getElementById('code_json');
+var code = "";
+var seu_codigo = "";
+
+function renderJson(data) {
+
+    code = `"${data.descricao}": {\n`;
+    code += `\t"prefix": "${data.atalho}",\n`;
+    code += `\t\t"body": [\n`;
+
+    var seu_codigo = data.seu_codigo;
+
+    var seu_codigoSplit = seu_codigo.split("\n");
+
+    var body = "";
+
+    seu_codigoSplit.forEach(element => {
+        console.log(element);
+        body += `\t\t\t"${element}",\n`;
+    });
+
+    /* console.log(seu_codigoSplit) */
+
+    code += `${body}\n`;
+    code += `\t\t],\n`;
+    code += `\t"description": "${data.descricao}"\n`;
+    code += `}`;
+
+    code_json.value = code;
+
+}
+
+
+
+
+
+/*  
+
+"${data.descricao}": {
+  "prefix": "${data.atalho}",
+  "body": [
+    "SNIPPED"
+  ],
+  "description": "${data.descricao}"
+}
+  
+*/
+
+/*
+inputs.forEach(function(field, index) {
+
+    pppoe[field.id] = field.value;
+
+});
+*/
